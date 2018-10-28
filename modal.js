afceaeea6c772970c7443fa9b7c512ca396a82fb
chibuike vincent <chibuikepatrick2@gmail.com>
2018-10-28 12:58:59 +0100

    done with the Login form

--- modal.js
+++ modal.js
@@ -24,7 +24,7 @@ window.onclick = function(event) {
     }
 }
 
-function openCity(evt, cityName) {
+function openCity(evt, action) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -34,12 +34,12 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(action).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
 // Get the modal
-var login_modal = document.getElementById('id01');
+var login_modal = document.getElementById('mylogin');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
