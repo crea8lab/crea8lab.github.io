6f7789e83adfd6e7ebb19b83867b44afd786bcf8
Fayvor George <phavorsparks@gmail.com>
2018-11-14 10:31:47 +0100

    Finished with the changeHeaderColor func

--- src/contact_us/contact_us.js
+++ src/contact_us/contact_us.js
@@ -2,13 +2,14 @@ const header = document.querySelector('header')
 const messenger = document.querySelector('.messenger')
 
 let messengerTop = messenger.offsetTop
-let headerHeight = header.offsetHeight
-
-console.log(headerHeight, messengerTop)
 
 const changeHeaderColor = () => {
-  if (messengerTop <= headerHeight) {
-    console.log('reached limit...')
+  const checkHeight = messengerTop - window.scrollY
+
+  if (checkHeight <= 60) {
+    header.classList.add('fixBgColor')
+  } else {
+    header.classList.remove('fixBgColor')
   }
 }
 
